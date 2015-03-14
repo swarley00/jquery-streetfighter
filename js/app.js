609dbb90396582ae74b831df4c8cf71161af6dcf
swarley00 <jason.caryl@gmail.com>
2015-03-14 13:57:34 -0400

    Add hadouken sound

--- js/app.js
+++ js/app.js
@@ -8,11 +8,25 @@ $(document).ready(function() {
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
-		//play hadouken sound
+		playHadouken();
 		$('.ryu-ready').hide();
 		$('.ryu-throwing').show();
-		$('.hadouken').show();
-		//show hadouken and animate it to the right of the screen
+		$('.hadouken').show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+		);
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+		);
 	})
 	.mouseup(function() {
 		//ryu goes back to his ready position
@@ -20,3 +34,9 @@ $(document).ready(function() {
 		$('.ryu-ready').show();
 	}); 
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
